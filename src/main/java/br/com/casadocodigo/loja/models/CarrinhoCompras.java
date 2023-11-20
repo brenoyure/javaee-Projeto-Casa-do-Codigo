48db8e80c07a43d0592fc7413c3303e1c70d0615
Breno Yuri <byurifb@gmail.com>
2023-11-20 10:02:08 -0300

    Serviço de Pagamento Rest implementado
    Criada uma chamada REST a um serviço de pagamento externo, utilizando o JAX-RS.

--- src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
+++ src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
@@ -15,6 +15,11 @@ import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.json.Json;
 import jakarta.json.JsonArrayBuilder;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.Invocation.Builder;
+import jakarta.ws.rs.client.WebTarget;
 
 @Named @SessionScoped
 public class CarrinhoCompras implements Serializable {
@@ -57,6 +62,18 @@ public class CarrinhoCompras implements Serializable {
 		compra.setUsuario(usuario);
 		compra.setItens(toJson());
 		compraDao.salvar(compra);
+
+		Client client = ClientBuilder.newClient();
+		Pagamento pagamento = new Pagamento(getTotal());
+		String target = "http://localhost:8080/payment/payment";
+		
+		Entity<Pagamento> json = Entity.json(pagamento);
+		WebTarget webTarget = client.target(target);
+		Builder requestBuilder = webTarget.request();
+		String response = requestBuilder.post(json, String.class);
+
+		System.out.println(response);
+
 	}
 
 	public String toJson() {
