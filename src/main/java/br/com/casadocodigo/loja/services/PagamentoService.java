a09d87caca95fd4f859c3225c1ab34bb37d2431e
Breno Yuri <byurifb@gmail.com>
2023-11-27 15:00:58 -0300

    Utilizando AsyncResponse
    Executando o pagamento de maneira assíncrona, utilizando o async response do JaxRS

--- src/main/java/br/com/casadocodigo/loja/services/PagamentoService.java
+++ src/main/java/br/com/casadocodigo/loja/services/PagamentoService.java
@@ -3,6 +3,8 @@ package br.com.casadocodigo.loja.services;
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
 import java.net.URI;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import br.com.casadocodigo.loja.daos.CompraDao;
 import br.com.casadocodigo.loja.models.Compra;
@@ -12,6 +14,8 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.container.AsyncResponse;
+import jakarta.ws.rs.container.Suspended;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriBuilder;
@@ -28,19 +32,24 @@ public class PagamentoService {
 	@Context
 	private ServletContext servletContext;
 
+	private static ExecutorService executor = Executors.newFixedThreadPool(50) ;
+
 	@POST
 	@Produces(APPLICATION_JSON)
-	public Response pagar(@QueryParam("uuid") String uuid) {
+	public void pagar(@Suspended final AsyncResponse ar, @QueryParam("uuid") String uuid) {
 		Compra compra = compraDao.buscaPorUuid(uuid);
-		pagamentoGateway.pagar(compra.getTotal());
 
-		URI responseUri = UriBuilder
-								.fromPath("http://localhost:8080" + servletContext.getContextPath() + "/index.xhtml")
-								.queryParam("msg", "Compra Realizada com Sucesso").build();
+		executor.submit(() -> {
+			pagamentoGateway.pagar(compra.getTotal());
 
-		Response response = Response.seeOther(responseUri).build();
+			URI responseUri = UriBuilder
+					.fromPath("http://localhost:8080" + servletContext.getContextPath() + "/index.xhtml")
+					.queryParam("msg", "Compra Realizada com Sucesso").build();
 
-		return response;
+			Response response = Response.seeOther(responseUri).build();
+			ar.resume(response);
+
+		});
 
 	}
 
